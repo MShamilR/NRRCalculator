2616ad80893742b25f96f1dbe7fb51c209deb5fb
supremeincubator <mohammedshamil0300@gmail.com>
2022-11-01 17:50:43 +0530

    DefaultNRR working, should add parse float to the calculations

--- index.js
+++ index.js
@@ -10,12 +10,42 @@ const defNRR = () => {
     let runRateYT
     let runRateYO
     let bowledYT
-    let bowledYO
+  let bowledYO
+  let defNRR
     for (const defBowledYT of defBowledYTs) {
       if (defBowledYT.checked) {
         bowledYT = defBowledYT.id;
         break;
       }
     }
-    console.log(bowledYT)
+    for (const defBowledYO of defBowledYOs) {
+      if (defBowledYO.checked) {
+        bowledYO = defBowledYO.id;
+        break;
+      }
+  }
+  if (bowledYT === "yes") {
+    const rrPerBall = defYTS / (defOPI*6)
+    runRateYT = rrPerBall * 6
+    console.log(runRateYT)
+  } if (bowledYT === "no") {
+    const rrPerBall = defYTS / (defYTO*6)
+    runRateYT = rrPerBall * 6;
+    console.log(runRateYT);
+  }
+  if (bowledYO === "yes") {
+    const rrPerBall = defYOS / (defOPI*6);
+    console.log(runRateYO);
+
+    runRateYO = rrPerBall * 6;
+    console.log(runRateYO);
+  }
+  if (bowledYO === "no") {
+    const rrPerBall = defYOS / (defYOO*6);
+    runRateYO = rrPerBall * 6;
+    console.log(runRateYO);
+
+  }
+  defNRR = runRateYT - runRateYO
+  console.log(defNRR)
 }
